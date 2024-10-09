da5654be66263a84018201880662739f3cadbfb4
16 <soup16-work@outlook.com>
2024-10-09 17:58:18 +0800

    feat: alias @config

--- config/vite/alias.ts
+++ config/vite/alias.ts
@@ -2,5 +2,6 @@ import path from 'path';
 import { AliasOptions } from 'vite';
 
 export const alias: AliasOptions = {
-    '@': path.resolve(__dirname, '../../src'),
+  '@': path.resolve(__dirname, '../../src'),
+  '@config': path.resolve(__dirname, '../../config'),
 };
