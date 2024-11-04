23c1ada7d909076a77fa838607f5e45eb4f965ea
16 <soup16-work@outlook.com>
2024-11-04 16:53:02 +0800

    perf: <InputSearch> value

--- src/components/Input/InputSearch.tsx
+++ src/components/Input/InputSearch.tsx
@@ -1,8 +1,8 @@
 /** Props */
 interface Props {
   value: string;
-  onChange: (newValue: string) => void;
-  onSearch: () => void;
+  onChange?: (newValue: string) => void;
+  onSearch?: (newValue: string) => void;
   placeholder?: string;
   className?: string;
 }
@@ -12,13 +12,19 @@ export const InputSearch = (props: Props) => {
   /** Params */
   const state = ahooks.reactive({
     isFocus: false,
+    value: props.value,
   });
 
   /** Actions */
+  // 同步值
+  useEffect(() => {
+    state.value = props.value;
+  }, [props.value]);
+
   // 聚焦时按回车键时搜索
   ahooks.updateEffect(() => {
     const onEnterPress = (event: KeyboardEvent) => {
-      if (event.key === 'Enter') props.onSearch();
+      if (event.key === 'Enter') props.onSearch && props.onSearch(state.value);
     };
 
     if (state.isFocus) window.addEventListener('keydown', onEnterPress);
@@ -34,8 +40,11 @@ export const InputSearch = (props: Props) => {
       <input
         className="input-1 w-full h-full pl-10 pr-40"
         placeholder={props.placeholder}
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
+        value={state.value}
+        onChange={(e) => {
+          state.value = e.target.value;
+          props.onChange && props.onChange(e.target.value);
+        }}
         onFocus={() => (state.isFocus = true)}
         onBlur={() => (state.isFocus = false)}
       />
@@ -44,7 +53,7 @@ export const InputSearch = (props: Props) => {
       <Svg
         name="search"
         className="w-20 absolute top-1/2 -translate-y-1/2 right-10 text-tip hover:text-primary cursor-pointer duration-300"
-        onClick={props.onSearch}
+        onClick={() => props.onSearch && props.onSearch(state.value)}
       />
     </div>
   );
