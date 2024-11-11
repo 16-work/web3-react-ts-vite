bc218fb4b78cf8efd61e0c41f115dadf6222fa33
16 <soup16-work@outlook.com>
2024-11-11 14:51:20 +0800

    fix: BoxCollapse isFold

--- src/components/Box/BoxCollapse.tsx
+++ src/components/Box/BoxCollapse.tsx
@@ -11,7 +11,7 @@ export const BoxCollapse = (props: Props) => {
   /** Template */
   return (
     <AnimatePresence>
-      {props.isFold && (
+      {!props.isFold && (
         <motion.div style={{ overflow: 'hidden' }} initial={{ height: 0 }} animate={{ height: 'auto' }} exit={{ height: 0 }} transition={{ duration: 0.3 }}>
           {props.children}
         </motion.div>
