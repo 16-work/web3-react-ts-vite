89ddc9d633fca3c93fbbbfac0da45855f433cf6c
16 <soup16-work@outlook.com>
2024-11-11 10:46:26 +0800

    perf: components auto import

---
+++ src/components/Feature/Demo.tsx
@@ -0,0 +1,12 @@
+/** Component */
+// 放在@/components内的组件，除了Feature目录下的，其余均会被auto-import收录
+export const Demo = () => {
+  /** Retrieval */
+
+  /** Params */
+
+  /** Actions */
+
+  /** Template */
+  return <div></div>;
+};
