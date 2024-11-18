76773834fb4cc7905dbc96352d55701f5d37fda9
16 <soup16-work@outlook.com>
2024-11-18 11:22:51 +0800

    perf: <PopoverText> props

--- src/components/Popover/PopoverText.tsx
+++ src/components/Popover/PopoverText.tsx
@@ -2,17 +2,17 @@ import { Popover as APopover, PopoverProps } from 'antd';
 
 /** Props */
 interface Props extends Omit<PopoverProps, 'content'> {
-  content: string;
-  contentClassName?: string;
+  tip: string;
+  tipClassName?: string;
 }
 
 /** Component */
 export const PopoverText = (props: Props) => {
   /** Template */
-  return props.content ? (
+  return props.tip ? (
     <APopover
       {...props}
-      content={<span className={`inline-block xs:max-w-300 md:max-w-600 px-10 py-5 text-common-1 thin ${props.contentClassName}`}>{props.content}</span>}
+      content={<span className={`inline-block xs:max-w-300 md:max-w-600 px-10 py-5 text-common-1 thin ${props.tipClassName}`}>{props.tip}</span>}
     >
       {/* 不套一层span的话，children为SvgIcon时会有警告 */}
       <span>{props.children}</span>
