5da9e3ce6ab7001f4ead000fde36c42b6fb0bcf5
16 <soup16-work@outlook.com>
2024-12-11 10:58:08 +0800

    perf: <Tabs>

--- src/components/Switch/Tabs.tsx
+++ src/components/Switch/Tabs.tsx
@@ -1,17 +1,39 @@
 import { Option } from '@/types/common';
 
+const styleType = {
+  base: {
+    box: 'w-fit p-5 border rounded-full',
+    initOffset: 5,
+    list: '',
+    tab: 'px-65 py-6 rounded-full text-black font-xl',
+    activeBg: 'bg-primary-1 rounded-full',
+    activeTab: '!text-common-1',
+  },
+  second: {
+    box: '',
+    initOffset: 0,
+    list: '',
+    tab: '',
+    activeBg: 'bg-primary-1',
+    activeTab: '',
+  },
+};
+
 /** Props */
 interface Props {
   value: any;
   options: Option[];
   onSwitch: (tab: any) => void;
 
+  styleType?: keyof typeof styleType;
   itemClassname?: string;
 }
 
 /** Component */
 export const Tabs = (props: Props) => {
   /** Params */
+  const type = props.styleType ?? 'base';
+
   const state = ahooks.reactive({
     colWidth: 0,
     colHeight: 0,
@@ -34,16 +56,16 @@ export const Tabs = (props: Props) => {
 
   /** Template */
   return (
-    <div className="w-fit relative p-6 rounded-8">
+    <div className={`w-fit relative ${styleType[type].box}`}>
       {/* active bg */}
       <div
-        className="absolute position-center-v z-0 bg-primary-1 rounded-8 duration-300"
-        style={{ width: state.colWidth, height: state.colHeight, left: activeIndex * state.colWidth + 5 }}
+        className={`absolute position-center-v z-0 duration-300 ${styleType[type].activeBg}`}
+        style={{ width: state.colWidth, height: state.colHeight, left: activeIndex * state.colWidth + styleType[type].initOffset }}
       ></div>
 
       {/* list */}
       <div
-        className={`relative z-[1] grid`}
+        className={`relative z-[1] grid ${styleType[type].list}`}
         style={{
           gridTemplateColumns: `repeat(${props.options.length}, minmax(0, 1fr))`,
         }}
@@ -53,9 +75,10 @@ export const Tabs = (props: Props) => {
           <div
             key={index}
             ref={index === 0 ? colItemRef : null}
-            className={`flex-align-x justify-center px-26 py-4 rounded-8 xs:text-20 md:text-18 duration-300 cursor-pointer whitespace-nowrap
+            className={`flex-align-x justify-center duration-300 cursor-pointer whitespace-nowrap
+              ${styleType[type].tab}
               ${props.itemClassname}
-              ${item.value === props.value ? 'text-common-1' : 'text-tip-1'}    
+              ${item.value === props.value && styleType[type].activeTab}    
             `}
             onClick={() => props.onSwitch(item.value)}
           >
