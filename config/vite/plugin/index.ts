2582e056ecfc8f27c446a50ebfe72edef289e971
16 <soup16-work@outlook.com>
2024-12-18 16:51:31 +0800

    feat: rollup-plugin-visualizer

--- config/vite/plugin/index.ts
+++ config/vite/plugin/index.ts
@@ -4,11 +4,13 @@ import { setMock } from './mock';
 import { setAutoImport } from './autoImport';
 import { setSvg } from './svg';
 import { setCompression } from './gzip.ts';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export const setPlugins = (env: MetaEnv) => {
   let plugins: PluginOption[] = [react()];
 
   if (env.VITE_ENV === 'mock') plugins.push(setMock());
+  plugins.push(visualizer());
   plugins.push(...setAutoImport());
   plugins.push(setSvg());
   plugins.push(setCompression());
