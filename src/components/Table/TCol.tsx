bc279bffcf4ad93fdb3374721cd799b6a4e3fb4a
16 <soup16-work@outlook.com>
2025-03-11 17:20:49 +0800

    fix: <TCol>

--- src/components/Table/TCol.tsx
+++ src/components/Table/TCol.tsx
@@ -15,7 +15,9 @@ export const TCol = (props: Props) => {
 
   /** Template */
   return (
-    <div className={`flex xs:flex-col md:flex-row justify-center xs:items-start md:items-center ${props.className}`}>
+    <div
+      className={`flex justify-center xs:items-start md:items-center ${props.alwaysFullyDisplay ? 'flex-row' : 'xs:flex-col md:flex-row'} ${props.className}`}
+    >
       {/* mobile label */}
       {screenType < SCREEN.MD && !props.alwaysFullyDisplay && <span className="mb-2 text-base text-tip-1 whitespace-nowrap">{props.label}</span>}
 
