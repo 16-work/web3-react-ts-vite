b6f568ea77877c047f34452cc999ccd0110f8c36
16 <soup16-work@outlook.com>
2025-03-20 14:39:57 +0800

    fix: some styles

--- src/layout/components/LayoutActions/ActionUser/BtnUser.tsx
+++ src/layout/components/LayoutActions/ActionUser/BtnUser.tsx
@@ -4,6 +4,7 @@ const baseClassName = 'xs:w-full md:w-fit xs:h-80 md:h-40 px-20 rounded-8 font-b
 export const BtnUser = () => {
   /** Retrieval */
   const account = useAccount();
+  const { t } = useTranslation();
 
   /** Template */
   return account.address ? (
@@ -11,12 +12,8 @@ export const BtnUser = () => {
     <button className={`btn-user    ${baseClassName} btn-primary `}>{format.address(account.address, 6, 4)}</button>
   ) : (
     // btn: connect
-    <Button
-      onClick={hooks.wallet.connect}
-      isLoading={account.isConnecting || account.isReconnecting}
-      className={`btn-connect    ${baseClassName} btn-primary `}
-    >
-      <span className="font-base">Connect</span>
+    <Button onClick={hooks.wallet.connect} isLoading={account.isConnecting || account.isReconnecting} className={`btn-connect    ${baseClassName} btn-primary`}>
+      {t('account.connect')}
     </Button>
   );
 };
