e97eca89894a373ce0fb04fd5ab262f96a7865b3
16 <soup16-work@outlook.com>
2025-03-28 18:34:02 +0800

    perf: zhTWConverter

--- src/constants/i18n/script/excelTolang.js
+++ src/constants/i18n/script/excelTolang.js
@@ -2,25 +2,11 @@
 import fs from "fs";
 import path from "path";
 import xlsx from "xlsx";
-import * as OpenCC from "opencc-js";
+import { converter } from './zhTWConverter.js';
 
 const commonFilePath = path.resolve("./", "src/constants/i18n/config.ts");
 const commonFileContent = fs.readFileSync(commonFilePath, "utf8");
-const converter = OpenCC.ConverterFactory(
-  OpenCC.Locale.from.cn,
-  OpenCC.Locale.to.tw.concat([
-    ["查看", "檢視"],
-    ["过滤", "篩選"],
-    ["服务器", "伺服器"],
-    ["文件", "檔案"],
-    ["拖动", "拖放"],
-    ["余额", "餘額"],
-    ["检索", "搜尋"],
-    ["倒计时", "倒數"],
-    ["分布", "分佈"],
-    ["字符", "字元"],
-  ])
-);
+
 // 读取需要生成的语言列表
 const match = commonFileContent.match(/supportLanguages\s*=\s*({[\s\S]*?});/);
 const languageOptions = new Function(`return ${match[1]}`)();
